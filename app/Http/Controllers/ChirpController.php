58a71c34d4e3ad115b448417b52c93e8da186c74
OSTX <othersyntax@gmail.com>
2026-04-28 14:40:58 +0800

    chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\View\View;
 
 class ChirpController extends Controller
 {
-    //
+    public function index(){
+        return view('chirps');
+        // return view('chirps',[]);
+    }
+
 }
